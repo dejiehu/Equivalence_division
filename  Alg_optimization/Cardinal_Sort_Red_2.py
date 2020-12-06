c0bfee61202794d5551d07c035f603483113f4b7
hudejie <1483513547@qq.com>
2020-12-06 13:39:56 +0800

    12.6 13:39

---  Alg_optimization/Cardinal_Sort_Red_2.py
+++  Alg_optimization/Cardinal_Sort_Red_2.py
@@ -60,13 +60,58 @@ def U_pos_nes(con_list,dec_data):    #求简化表
                 Upos_list.append(i[0])
     return Upos_list,Uneg_list
 
+def is_belongTo(con_list,Upos_list):  #判断是否属于
+    for i in con_list:
+        if not Upos_list.__contains__(i):
+            return False
+    return True
+
+def is_card_yes(con_list,dec_data):   # 判断基数是否为1
+    for i in con_list:
+        if dec_data[i] !=dec_data[0]:
+            return False
+    return True
+
+def cal_Up(U_list,P_data,Upos_list,Uneg_list,dec_data):
+    X = div(P_data,U_list)
+    Up = []
+    for i_list in X:
+        if is_belongTo(i_list,Upos_list) & is_card_yes(i_list,dec_data):
+            Up = Up + i_list
+        elif is_belongTo(i_list,Uneg_list):
+            Up = Up + i_list
+    return Up
+
+def calculate(U_list,P_data,attr_data,Upos_list,Uneg_list,dec_data):
+
+    if len(P_data) == 0:
+        X_list = [U_list.copy()]
+    else:
+        Up_list = cal_Up(U_list,P_data,Upos_list,Uneg_list,dec_data)
+        X_list = div(P_data,list(set(U_list).difference(set(Up_list))))    #(U′ - Up′)/a
+    sig_list = []
+    Bp_list = []
+    NBp_list = []
+    UPa_divlist = div(attr_data,list(chain.from_iterable(X_list)))
+    for i_list in UPa_divlist:
+        if is_belongTo(i_list,Upos_list) & is_card_yes(i_list,dec_data):
+            Bp_list = Bp_list + i_list
+        if is_belongTo(i_list,Uneg_list):
+            NBp_list = NBp_list + i_list
+    sig_list = Bp_list + NBp_list
+    print(Bp_list , NBp_list)
+
 def Reduce_basedSig(my_data):
     con_data = deal_data(my_data, my_data.shape[1] - 1, my_data.shape[1] - 1)
     U_list = [i for i in range(len(my_data))]
     dec_data = deal_data(my_data, 0, my_data.shape[1] - 2)
     con_list = div(con_data,U_list)      #U/C
     Upos_list,Uneg_list = U_pos_nes(con_list, dec_data)   #   U'pos    U'neg
-
+    U_list = Upos_list +Uneg_list
+    P_data = []
+    attr_data = deal_data(con_data,3 , con_data.shape[1] - 1)
+    attr_data = deal_data(attr_data, 0,1 )
+    calculate(U_list,P_data,attr_data,Upos_list,Uneg_list,dec_data)
 
 if __name__ == '__main__':
     start = time.perf_counter()
