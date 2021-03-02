caaaf424339423fd57749fadc32e19272114a310
hudejie <1483513547@qq.com>
2021-03-02 17:50:37 +0800

    增量式的约简

--- An_incremental_approach_for_attribute_reduction_based_on_knowledge_granularity/An_incremental_Algorithm_1_granularity_Attribute_Reduction(IARC).py
+++ An_incremental_approach_for_attribute_reduction_based_on_knowledge_granularity/An_incremental_Algorithm_1_granularity_Attribute_Reduction(IARC).py
@@ -61,48 +61,50 @@ def granularity(con_divlist): #知识粒
 def condition_granularity(dec_data,con_data):  #相对条件粒计算
     return granularity(div(con_data)) - granularity(div(numpy.append(con_data,dec_data,axis=1)))
 
-def Core(dec_data,con_data):  #求核
-    core_data = numpy.empty(shape=(len(con_data), 0))
-    core_data = core_data.astype(int)
-    core = []
-    for i in range(con_data.shape[1]):
-        temp_core_data = deal_data(con_data,i,i)
-        if condition_granularity(dec_data,temp_core_data) - condition_granularity(dec_data,con_data) != 0:
-            core_data = numpy.append(core_data, con_data[:, i, numpy.newaxis], axis=1)
-            core += [i]
-    # print(core)
-    return core_data,core
+# def Core(dec_data,con_data):  #求核
+#     core_data = numpy.empty(shape=(len(con_data), 0))
+#     core_data = core_data.astype(int)
+#     core = []
+#     for i in range(con_data.shape[1]):
+#         temp_core_data = deal_data(con_data,i,i)
+#         if condition_granularity(dec_data,temp_core_data) - condition_granularity(dec_data,con_data) != 0:
+#             core_data = numpy.append(core_data, con_data[:, i, numpy.newaxis], axis=1)
+#             core += [i]
+#     # print(core)
+#     return core_data,core
 
-def red(dec_data, con_data):# 求约简
-    red_data,red_num = Core(dec_data,con_data)
-    if len(red_num) == 0:
-        print("无约简")
-        return
-    attr_data = numpy.empty(shape=(len(con_data), 0))
-    attr_data = attr_data.astype(int)
-    attr_num = []
-    for i in range(con_data.shape[1]):
-        if not(red_num.__contains__(i)):
-            attr_data = numpy.append(attr_data, con_data[:, i, numpy.newaxis], axis=1)
-            attr_num += [i]
-    while condition_granularity(dec_data,con_data) != condition_granularity(dec_data,red_data):
-        dict = {}
-        con_key = -1  # 字典key
-        con_value = -1  # 字典value
-        for i in range(attr_data.shape[1]):
-            temp_red_data = red_data
-            temp_red_data = numpy.append(temp_red_data, attr_data[:, i, numpy.newaxis], axis=1)
-            dict[i] = condition_granularity(dec_data,red_data) - condition_granularity(dec_data,temp_red_data)
-        print(dict)
-        for key in dict:
-            if dict[key] > con_value:
-                con_value = dict[key]
-                con_key = key
-        red_data = numpy.append(red_data,attr_data[:,con_key,numpy.newaxis],axis=1)
-        red_num += [attr_num[con_key]]
-        attr_data = deal_data(attr_data, con_key, con_key)
-        del attr_num[con_key]
-    return red_data,red_num
+def red(U_dec_data, U_con_data,Ux_dec_data,Ux_con_data,Ux_red_data):# 求约简
+    print(condition_granularity(Ux_dec_data, Ux_con_data),condition_granularity(Ux_dec_data, Ux_red_data))
+
+    # red_data,red_num = Core(dec_data,con_data)
+    # if len(red_num) == 0:
+    #     print("无约简")
+    #     return
+    # attr_data = numpy.empty(shape=(len(con_data), 0))
+    # attr_data = attr_data.astype(int)
+    # attr_num = []
+    # for i in range(con_data.shape[1]):
+    #     if not(red_num.__contains__(i)):
+    #         attr_data = numpy.append(attr_data, con_data[:, i, numpy.newaxis], axis=1)
+    #         attr_num += [i]
+    # while condition_granularity(dec_data,con_data) != condition_granularity(dec_data,red_data):
+    #     dict = {}
+    #     con_key = -1  # 字典key
+    #     con_value = -1  # 字典value
+    #     for i in range(attr_data.shape[1]):
+    #         temp_red_data = red_data
+    #         temp_red_data = numpy.append(temp_red_data, attr_data[:, i, numpy.newaxis], axis=1)
+    #         dict[i] = condition_granularity(dec_data,red_data) - condition_granularity(dec_data,temp_red_data)
+    #     print(dict)
+    #     for key in dict:
+    #         if dict[key] > con_value:
+    #             con_value = dict[key]
+    #             con_key = key
+    #     red_data = numpy.append(red_data,attr_data[:,con_key,numpy.newaxis],axis=1)
+    #     red_num += [attr_num[con_key]]
+    #     attr_data = deal_data(attr_data, con_key, con_key)
+    #     del attr_num[con_key]
+    # return red_data,red_num
 
 def De_redundancy(red_data,red_num,dec_data,con_data):# 去冗余
     i = 0
@@ -119,7 +121,7 @@ def De_redundancy(red_data,red_num,dec_data,con_data):# 去冗余
     print(red_num)
     return red_data,red_num
 
-def Add_Ux_dataShape(U_data,Ux_divlist):
+def Add_Ux_dataShape(U_data,Ux_divlist):   #  调整增加的属性的对象序号
     for i in range(len(Ux_divlist)):
         for j in range(len(Ux_divlist[i])):
             Ux_divlist[i][j] += U_data.shape[0]
@@ -132,7 +134,7 @@ def cal_red_divlist(red_num,con_data):   #根据核属性数值计算核属性�
         red_data =  numpy.append(red_data,con_data[:,i,numpy.newaxis],axis=1)
     return red_data
 
-def merge_divlist(U_divlist,Ux_divlist,U_data,Ux_data):
+def merge_divlist(U_divlist,Ux_divlist,U_data,Ux_data):#      U/C + Ux/C
     U_Ux_divlist = []
     for i in range(len(Ux_divlist)-1,-1,-1):
         for j in range(len(U_divlist)-1,-1,-1):
@@ -164,7 +166,8 @@ if __name__ == '__main__':
     Ux_red_divlist = Add_Ux_dataShape(U_red_data, div(Ux_red_data))
     Ck,CU_Ux_divlist = merge_divlist(U_con_divlist.copy(), Ux_con_divlist.copy(), U_con_data, Ux_con_data)
     REDk,RED_U_Ux_divlist = merge_divlist(U_red_divlist.copy(), Ux_red_divlist.copy(), U_red_data, Ux_red_data)
-    print(CU_Ux_divlist)
-    print(RED_U_Ux_divlist)
+    print(CU_Ux_divlist,Ck)
+    print(RED_U_Ux_divlist,REDk)
+    red(U_dec_data, U_con_data, Ux_dec_data, Ux_con_data,Ux_red_data)
     # red_data,red_num = red(dec_data, con_data)
     # red_data,red_num = De_redundancy(red_data, red_num, dec_data, con_data)
