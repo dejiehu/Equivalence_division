e396a88fe7e01c9e7efb212d10403fb375207d7e
hudejie <1483513547@qq.com>
2021-03-08 17:26:28 +0800

    zneglaingshi

--- An_incremental_approach_for_attribute_reduction_based_on_knowledge_granularity/An_incremental_Algorithm_1_Matrix_granularity_Attribute_Reduction(MIARC).py
+++ An_incremental_approach_for_attribute_reduction_based_on_knowledge_granularity/An_incremental_Algorithm_1_Matrix_granularity_Attribute_Reduction(MIARC).py
@@ -77,6 +77,7 @@ def U_Ux_condition_granularity(U_dec_data,Ux_dec_data,U_con_data,Ux_con_data):  
 def red(U_dec_data, U_con_data,Ux_dec_data,Ux_con_data,U_red_data, Ux_red_data, RED):# 求约简
     if U_Ux_condition_granularity(U_dec_data,Ux_dec_data,U_con_data,Ux_con_data) \
             == U_Ux_condition_granularity(U_dec_data,Ux_dec_data,U_red_data,Ux_red_data):
+        print("123456")
         return RED
     else:
         attr_num = []
@@ -121,41 +122,6 @@ def red(U_dec_data, U_con_data,Ux_dec_data,Ux_con_data,U_red_data, Ux_red_data, 
             Ux_attr_data = deal_data(Ux_attr_data, con_key, con_key)
             del attr_num[con_key]
         return RED, U_red_data, Ux_red_data
-
-
-
-
-
-    # red_data,red_num = Core(dec_data,con_data)
-    # if len(red_num) == 0:
-    #     print("无约简")
-    #     return
-    # attr_data = numpy.empty(shape=(len(con_data), 0))
-    # attr_data = attr_data.astype(int)
-    # attr_num = []
-    # for i in range(con_data.shape[1]): # 选出C-core（attr_data）
-    #     if not(red_num.__contains__(i)):
-    #         attr_data = numpy.append(attr_data, con_data[:, i, numpy.newaxis], axis=1)
-    #         attr_num += [i]
-    #
-    # while condition_granularity(dec_data,con_data) != condition_granularity(dec_data,red_data):
-    #     dict = {}
-    #     con_key = -1  # 字典key
-    #     con_value = -1  # 字典value
-    #     for i in range(attr_data.shape[1]):
-    #         temp_red_data = red_data
-    #         temp_red_data = numpy.append(temp_red_data, attr_data[:, i, numpy.newaxis], axis=1)
-    #         dict[i] = condition_granularity(dec_data,red_data) - condition_granularity(dec_data,temp_red_data)
-    #     print(dict)
-    #     for key in dict:
-    #         if dict[key] > con_value:
-    #             con_value = dict[key]
-    #             con_key = key
-    #     red_data = numpy.append(red_data,attr_data[:,con_key,numpy.newaxis],axis=1)
-    #     red_num += [attr_num[con_key]]
-    #     attr_data = deal_data(attr_data, con_key, con_key)
-    #     del attr_num[con_key]
-    # return red_data,red_num
 
 def De_redundancy(U_dec_data, U_con_data,Ux_dec_data,Ux_con_data,U_red_data,Ux_red_data,RED):# 去冗余
     GP = U_Ux_condition_granularity(U_dec_data, Ux_dec_data, U_con_data, Ux_con_data)
