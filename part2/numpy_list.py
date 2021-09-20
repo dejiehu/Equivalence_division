cb7aebb5f7f9cf06b2deeadb6bbb080925b62ee4
dejiehu <1483513547@qq.com>
2021-09-20 09:27:56 +0800

    11

--- part2/numpy_list.py
+++ part2/numpy_list.py
@@ -2,7 +2,7 @@ import random
 import time
 from itertools import chain
 import numpy
-shape_0 = 100
+shape_0 = 10
 shape_1 = 10
 start1 = time.perf_counter()
 list1 = []
@@ -14,41 +14,69 @@ for i in range(shape_0):
     list1.append(list2)
 # print(list1)
 a = 0
-for i in list1:
-    for k in list1:
-        if i == k:
-            print(i)
-            a+=1
 # for i in list1:
-#     for j in i:
-#         for k in list1:
-#             for l in k:
-#                 if j == l:
-#                     a += 1
+#     for k in list1:
+#         if i == k:
+#             print(i)
+#             a+=1
+
+for i in list1:
+    for j in i:
+        for k in list1:
+            for l in k:
+                if j == l:
+                    a += 1
+print(a)
 end1 = time.perf_counter()
 print("time1:",end1 - start1)
 
 start2 = time.perf_counter()
-core_data = numpy.empty(shape=(100,10))
+core_data = numpy.empty(shape=(10,10))
 # print(core_data)
 for i in range((core_data.shape[0])):
     for j in range((core_data.shape[1])):
         core_data[i][j] = random.randint(1,10)
 # print(core_data)
 a = 0
-for i in range((core_data.shape[0])):
-    for j in range((core_data.shape[0])):
-        if (core_data[i] == core_data[j]).all():
-            a+=1
 # for i in range((core_data.shape[0])):
-#     for j in range((core_data.shape[1])):
-#         for k in range((core_data.shape[0])):
-#             for l in range((core_data.shape[1])):
-#                 if core_data[i][j] == core_data[k][l]:
-#                     a += 1
+#     for j in range((core_data.shape[0])):
+#         if (core_data[i] == core_data[j]).all():
+#             a+=1
+for i in range((core_data.shape[0])):
+    for j in range((core_data.shape[1])):
+        for k in range((core_data.shape[0])):
+            for l in range((core_data.shape[1])):
+                if core_data[i][j] == core_data[k][l]:
+                    a += 1
 
 # div_list = [[1,2,4,5,7,6,8,2,8,1,6,8,1,6]]
 # for i  in range(2000):
     # list(chain.from_iterable(div_list))
 end2 = time.perf_counter()
 print("time2:",end2 - start2)
+
+def readfile(filename):
+    file = open(filename,"r")
+    list_text = []
+    for line in file.readlines():
+        # print(line)
+        line = list(line.strip().split('\t'))
+        s= []
+        for i in line:
+            s.append(i)
+        list_text.append(s)
+    print(list_text)
+    return list_text
+readfile("../data.txt")
+end3 = time.perf_counter()
+print(end3-end2)
+def readfile1():
+    my_data = numpy.loadtxt('../data.txt')
+    print(my_data)
+    return my_data
+readfile1()
+end4 = time.perf_counter()
+print(end4-end3)
+# print(readfile1("../data.txt"),"0000")
+# if readfile("../Car Evaluation.txt")[1] == readfile1()[1]:
+#     print("==")
