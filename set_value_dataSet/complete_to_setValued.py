b42d916791dfec360f417e08313e08d085c02ac4
hudejie <1483513547@qq.com>
2021-12-28 17:08:05 +0800

    shuju

---
+++ set_value_dataSet/complete_to_setValued.py
@@ -0,0 +1,42 @@
+import random
+
+
+def readfileBylist(filename):
+    file = open(filename,"r")
+    list_row = file.readlines()
+    list_data = []
+    for i in range(len(list_row)):
+        list_line = list_row[i].strip().split('\t')
+        list_data.append(list_line)
+    return list_data
+
+if __name__ == '__main__':
+    list_data = readfileBylist("../complete_dataSet_classication/Cardiotocography.txt")
+    print(len(list_data),"对象数")
+    print(len(list_data[0])-1,"条件属性数")
+    con_data = list(map(lambda x: x[:(len(list_data[0]) - 1)], list_data))
+    dec_data = list(map(lambda x: x[(len(list_data[0]) - 1):], list_data))
+    attr_all_set = []
+    for i in range(len(list_data[0])):
+        attr_set = set()
+        for j in range(len(list_data)):
+            attr_set.add(int(list_data[j][i]))
+        attr_all_set.append(attr_set)
+    ratio = int((len(list_data) * len(con_data[0])) * 0.1)
+    print(ratio,"ratio",len(list_data) * len(con_data[0]))
+    for i in range(ratio):
+        attr_num = random.randint(0,len(con_data[0])-1)
+        list_data[random.randint(0,len(list_data)-1)][attr_num] = attr_all_set[attr_num]
+    with open('Cardiotocography_setValued.txt', 'w') as f:
+        for i in range(len(list_data)):
+            for j in range(len(list_data[0])):
+                if j != len(con_data[0]):
+                    if isinstance(list_data[i][j],set):
+                        f.write(repr(list_data[i][j]) + " ")
+                    else:
+                        f.write("{" + list_data[i][j] + "}" + " ")
+                else:
+                    f.write(list_data[i][j])
+            if i != len(con_data)-1:
+                f.write("\n")
+    f.close()
